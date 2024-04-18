1da056a86d11f270b092e4d30eeaf6a0df14dac7
soonmo <104075292+Soonmo97@users.noreply.github.com>
2024-04-18 18:39:10 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2556 ms, Memory: 417008 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 10989 오름차순 정렬
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+        // 수의 개수 입력 및 배열 생성
+        int[] arr = new int[Integer.parseInt(br.readLine())];
+
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int a : arr) {
+            sb.append(a + "\n");
+        }
+        System.out.println(sb);
+    }
+}
