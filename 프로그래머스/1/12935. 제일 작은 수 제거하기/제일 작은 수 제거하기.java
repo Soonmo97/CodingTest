a7d604f57f11508f158a6b52384e2ea376cf533d
Soonmo Kwon <104075292+Soonmo97@users.noreply.github.com>
2025-05-04 13:43:39 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.66 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -9,7 +9,7 @@ class Solution {
         }
         int idx = 0;
         for (int i=0; i<arr.length; i++) {
-            if (arr[i] > min) answer[idx++] = arr[i];
+            if (arr[i] != min) answer[idx++] = arr[i];
         }
         
         return answer;
