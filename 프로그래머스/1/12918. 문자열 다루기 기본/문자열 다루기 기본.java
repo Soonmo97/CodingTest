8538ed5702cb30dee2329e2a335592aebe155c95
Soonmo Kwon <104075292+Soonmo97@users.noreply.github.com>
2025-05-04 12:38:40 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = false;
+        boolean answer = true;
+        
+        if (s.length() != 4 && s.length() != 6) return answer = false;
+        
         char[] c = s.toCharArray();
-        if (s.length() == 4 || s.length() == 6) {
-            for (char item : c) {
-                int n = (int)item;
-                if (n >= 48 && n <= 57) {
-                    answer = true;
-                } else {
-                    answer = false;
-                    break;
-                }
-            }
-        }  
+        
+        for (char item : c) {
+            if (!Character.isDigit(item)) return answer = false;
+        }
         
         return answer;
     }
